237fb6f45f57c0633d58bd6508b773876be6b72d
vinzmc <maurice.marvin@student.umn.ac.id>
2021-06-01 23:26:35 +0700

    minor fix
    - fixing wrong page name
    - changin PNG filename to png
    - removing user image link to aboutus image

--- application/views/pages/cart/cartDetail.php
+++ application/views/pages/cart/cartDetail.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Finished Transaction</title>
+    <title>Booking Form</title>
     <link rel="icon" href="<?= base_url() ?>/favicon.svg" type="image/gif">
     <?php echo $style ?>
 </head>
